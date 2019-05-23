b0315a99a23dc8a77ae4dc1c94c50fb160696275
cubsfan46350 <cubsfan.46350@msn.com>
2019-05-23 12:56:32 -0500

    implement part of OAuth on frontend

--- background_check/src/App.js
+++ background_check/src/App.js
@@ -8,9 +8,10 @@ import Candidates from './components/candidates/candidateForm';
 import CandidateList from './components/candidates/candidateList';
 import Login from './Login';
 import Register from './Register';
-
+import { GoogleLogin } from 'react-google-login';
 import {Elements, StripeProvider} from 'react-stripe-elements';
 import CheckoutForm from './CheckoutForm';
+const keys = require('./keys');
 
 
 
@@ -21,8 +22,76 @@ const Homepage = styled.div`
 `;
 
 class App extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      isAuthenticated: false,
+      user: null,
+      token: ''
+    };
+  }
+
+  logout = () => {
+    this.setState({
+      isAuthenticated: false,
+      token: '',
+      user: null
+    })
+  };
+
+  googleResponse = (response) => {
+    const tokenBlob = new Blob([JSON.stringify({
+      access_token: response.accessToken
+    }, null, 2)], {type : 'application/json'});
+    const options = {
+      method: 'POST',
+      body: tokenBlob,
+      mode: 'cors',
+      cache: 'default'
+    };
+    fetch('http://localhost:4000/api/v1/auth/google', options).then(
+      r => {
+        const token = r.headers.get('x-auth-token');
+        r.json().then(user => {
+          if (token) {
+            this.setState({isAuthenticated: true, user, token})
+          }
+        });
+      }
+    )
+  };
+
+  onFailure = (error) => {
+    alert(error);
+  };
   
   render() {
+
+    let content = !!this.state.isAuthenticated ?
+      (
+        <div>
+          <p>Authenticated</p>
+          <div>
+            {this.state.user.email}
+          </div>
+          <div>
+            <button onClick={this.logout} className="button">
+              Log Out
+            </button>
+          </div>
+        </div>
+      ) :
+      (
+        <div>
+          <GoogleLogin 
+            client_id = {keys.google.clientID}
+            buttonText = "Login"
+            onSuccess={this.googleResponse}
+            onFailure={this.onFailure}
+          />
+        </div>
+      );
 
     return (
       <Router>
@@ -42,6 +111,7 @@ class App extends React.Component {
             <Link to ='/candidates'>Candidates</Link>
             <Link to ='/candidateList'>Submit Request</Link>
           </Homepage>
+          {content}
           
             
             <Route exact path= '/login' component={Login} />
---
+++ background_check/src/keys.js
@@ -0,0 +1,7 @@
+const CLIENT_ID=process.env.GOOGLE_CLIENT_ID;
+
+module.exports = {
+    google: {
+        clientID: CLIENT_ID
+    }
+}
